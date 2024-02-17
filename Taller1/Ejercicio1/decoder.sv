6eab9601dafe1e8055aa34249815ba079af80e5f
ItsRichard7 <ricardobm@estudiantec.cr>
2024-02-17 14:27:47 -0600

    Finalización Ejercicio #1

--- Taller1/Ejercicio1/decoder.sv
+++ Taller1/Ejercicio1/decoder.sv
@@ -1,17 +1,17 @@
-module decoder (input A,B,C,D, output a,b,c,d,e,f,g);
+module decoder(input logic A,B,C,D, output logic a,b,c,d,e,f,g);
 	
-	assign a = ~A & ~B | ~B & ~C | ~A & D | B & C | A & ~D|;
+	assign a = ~(~B & ~D | ~A & C | B & C | A & ~D | ~A & B & D | A & ~B & ~C);
 	
-	assign b = ~A & ~B | ~B & ~D | ~A & ~C & ~D | ~A & C & D | A & ~C & D;
+	assign b = ~(~A & ~B | ~B & ~D | ~A & ~C & ~D | ~A & C & D | A & ~C & D);
 	
-	assign c = ~A & B | A & ~B | ~A & ~C & ~D | ~A & C & D | B & ~C & D;
+	assign c = ~(~A & ~C | ~A & D | ~C & D | ~A & B | A & ~B);
 	
-	assign d = A & ~C | ~A & ~B & ~D | ~B & C & D | B & ~C & D | B & C & ~D;
+	assign d = ~(A & ~C | ~A & ~B & ~D | ~B & C & D | B & ~C & D | B & C & ~D);
 	
-	assign e = ~B & ~D | C & ~D | A & C | A & B;
+	assign e = ~(~B & ~D | C & ~D | A & C | A & B);
 	
-	assign f = ~C & ~D | B & ~D | A & ~B | A & C | ~A & B & ~C;
+	assign f = ~(~C & ~D | B & ~D | A & ~B | A & C | ~A & B & ~C);
 	
-	assign g = ~B & C | C & ~D | A & ~B | A & D |  ~A & B & ~C;
+	assign g = ~(~B & C | C & ~D | A & ~B | A & D |  ~A & B & ~C);
 	
 endmodule
--- Taller1/Ejercicio1/decoder_tb.sv
+++ Taller1/Ejercicio1/decoder_tb.sv
@@ -2,47 +2,47 @@ module decoder_tb();
 
 	logic A,B,C,D,a,b,c,d,e,f,g;
 
-	Decoder decoder(A,B,C,D,a,b,c,d,e,f,g);
+	decoder decoder(A,B,C,D,a,b,c,d,e,f,g);
 	
 	initial begin
-		A = 0
-		B = 0
-		C = 0
-		D = 0
+		A = 0;
+		B = 0;
+		C = 0;
+		D = 0;
 		#50
-		A = 0
-		B = 0
-		C = 0
-		D = 1
+		A = 0;
+		B = 0;
+		C = 0;
+		D = 1;
 		#50
-		A = 0
-		B = 0
-		C = 1
-		D = 0
+		A = 0;
+		B = 0;
+		C = 1;
+		D = 0;
 		#50
-		A = 0
-		B = 0
-		C = 1
-		D = 1
+		A = 0;
+		B = 0;
+		C = 1;
+		D = 1;
 		#50
-		A = 0
-		B = 1
-		C = 0
-		D = 0
+		A = 0;
+		B = 1;
+		C = 0;
+		D = 0;
 		#50
-		A = 0
-		B = 1
-		C = 0
-		D = 1
+		A = 0;
+		B = 1;
+		C = 0;
+		D = 1;
 		#50
-		A = 0
-		B = 1
-		C = 1
-		D = 0
+		A = 0;
+		B = 1;
+		C = 1;
+		D = 0;
 		#50
-		A = 0
-		B = 1
-		C = 1
-		D = 1
+		A = 0;
+		B = 1;
+		C = 1;
+		D = 1;
 	end 
 endmodule
