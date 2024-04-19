bee840122b185534c3a009e3cab63a5ccbdf01bd
ItsRichard7 <ricardobm@estudiantec.cr>
2024-04-18 18:43:55 -0600

    Changes

--- Laboratorio3/Code/comparatorLessThan.sv
+++ Laboratorio3/Code/comparatorLessThan.sv
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module comparatorLessThan #(parameter n = 3) (
 input logic[2:0] a, b, output logic res);
 
@@ -15,4 +16,23 @@ logic resTMP;
 	 
 	 assign res = resTMP;
 	 
+=======
+module comparatorLessThan #(parameter n = 3) (
+input logic[2:0] a, b, output logic res);
+
+logic resTMP;
+
+    always @* begin
+      if (a < b) begin
+         resTMP = 1;
+      end
+      else begin
+        
+		   resTMP = 0;
+      end
+    end
+	 
+	 assign res = resTMP;
+	 
+>>>>>>> parent of 4520d45 (Modificaciones)
 endmodule
