1bb8282a61bd7b91a504f2f55bf6a316b7e4f280
Carlos Rodríguez <c.rodriguez@estudiantec.cr>
2024-04-18 21:24:57 -0600

    Se agrega más al battleship_tb
    :cocktail: Se arregla la testbench del proyecto.

--- Laboratorio3/Code/Battleship_tb.sv
+++ Laboratorio3/Code/Battleship_tb.sv
@@ -3,25 +3,53 @@ module Battleship_tb;
 logic clk, rstSwitch, rowButton, colButton, selectButton;
 logic [6:0] rowSeg, colSeg, pcBoatsLeft, playerBoatsLeft;
 logic [9:0] message;
+logic [2:0] rowCoord, colCoord; // Declaración de las variables de coordenadas
 
 Battleship battleship(
-	.clk(clk), .rstSwitch(rstSwitch), .rowButton(rstSwitch), .colButton(rstSwitch), 
-	.selectButton(selectButton), .rowSeg(rowSeg), .colSeg(colSeg), .pcBoatsLeft(pcBoatsLeft), 
-	.playerBoatsLeft(playerBoatsLeft), .message(message)
+    .clk(clk), .rstSwitch(rstSwitch), .rowButton(rowButton), .colButton(colButton), 
+    .selectButton(selectButton), .rowSeg(rowSeg), .colSeg(colSeg), .pcBoatsLeft(pcBoatsLeft), 
+    .playerBoatsLeft(playerBoatsLeft), .message(message)
 );
 
 always begin
-		#1000 clk = ~clk;
+    #5 clk = ~clk;
 end
 
 initial begin
-	clk = 1;
-	rstSwitch = 1;
-	rowButton = 0;
-	colButton = 0;
-	selectButton = 0;
-	#20;
-	rstSwitch = 0;
+    clk = 1;
+    rstSwitch = 1;
+    rowButton = 0;
+    colButton = 0;
+    selectButton = 0;
+    #20;
+    rstSwitch = 0;
+
+    // Disparar a todas las casillas del juego
+    for (int row = 0; row < 5; row = row + 1) begin
+        for (int col = 0; col < 5; col = col + 1) begin
+            // Configurar fila y columna
+            rowCoord = row;
+            colCoord = col;
+
+            // Presionar el botón de fila
+            rowButton = 1;
+            #20;
+            rowButton = 0;
+            #20;
+
+            // Presionar el botón de columna
+            colButton = 1;
+            #20;
+            colButton = 0;
+            #20;
+
+            // Presionar el botón de selección
+            selectButton = 1;
+            #10;
+            selectButton = 0;
+            #20;
+        end
+    end
 end
 
 endmodule
