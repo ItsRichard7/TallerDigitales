539989ab344bd500b25ab500dae3d47f6e6e7456
FelipeJU22 <felipejimenezu03@gmail.com>
2024-06-11 14:46:13 -0600

    Últimos cambios en la VGA para subrayas palabras

--- Proyecto/VGA/changerom.sv
+++ Proyecto/VGA/changerom.sv
@@ -10,7 +10,7 @@ module changeRom(
 logic [7:0] charrom[0:2399]; // Generador del almacenador de Caracter ROM unidimensional
 
 logic [7:0] line; // Una línea leída por la ROM
-logic [4:0] q;
+logic [6:0] q;
 logic [9:0] address;
 
 logic [7:0] s, r; // Outputs de las RAMs
@@ -34,9 +34,9 @@ SecondRam ram2(.address(address), .clock(clk), .data(), .wren(wren), .q(r));
 // Multiplexor para seleccionar la salida de la RAM en función del switch
 always_comb begin
     if (switch) begin
-        q = r[4:0];
+        q = r[6:0];
     end else begin
-        q = s[4:0];
+        q = s[6:0];
     end
 end
 
@@ -44,34 +44,62 @@ end
 always_comb begin
     if (posy >= 0 && posy < 64) begin
         case (q)
-            5'd1: line = charrom[yoff + {ch-65, 3'b000 + 8*1 }];  // A
-            5'd2: line = charrom[yoff + {ch-65, 3'b000 + 8*2 }];  // B
-            5'd3: line = charrom[yoff + {ch-65, 3'b000 + 8*3 }];  // C
-            5'd4: line = charrom[yoff + {ch-65, 3'b000 + 8*4 }];  // D
-            5'd5: line = charrom[yoff + {ch-65, 3'b000 + 8*5 }];  // E
-            5'd6: line = charrom[yoff + {ch-65, 3'b000 + 8*6 }];  // F
-            5'd7: line = charrom[yoff + {ch-65, 3'b000 + 8*7 }];  // G
-            5'd8: line = charrom[yoff + {ch-65, 3'b000 + 8*8 }];  // H
-            5'd9: line = charrom[yoff + {ch-65, 3'b000 + 8*9 }];  // I
-            5'd10: line = charrom[yoff + {ch-65, 3'b000 + 8*10 }]; // J
-            5'd11: line = charrom[yoff + {ch-65, 3'b000 + 8*11 }]; // K
-            5'd12: line = charrom[yoff + {ch-65, 3'b000 + 8*12 }]; // L
-            5'd13: line = charrom[yoff + {ch-65, 3'b000 + 8*13 }]; // M
-            5'd14: line = charrom[yoff + {ch-65, 3'b000 + 8*14 }]; // N
-            5'd15: line = charrom[yoff + {ch-65, 3'b000 + 8*15 }]; // O
-            5'd16: line = charrom[yoff + {ch-65, 3'b000 + 8*16 }]; // P
-            5'd17: line = charrom[yoff + {ch-65, 3'b000 + 8*17 }]; // Q
-            5'd18: line = charrom[yoff + {ch-65, 3'b000 + 8*18 }]; // R
-            5'd19: line = charrom[yoff + {ch-65, 3'b000 + 8*19 }]; // S
-            5'd20: line = charrom[yoff + {ch-65, 3'b000 + 8*20 }]; // T
-            5'd21: line = charrom[yoff + {ch-65, 3'b000 + 8*21 }]; // U
-            5'd22: line = charrom[yoff + {ch-65, 3'b000 + 8*22 }]; // V
-            5'd23: line = charrom[yoff + {ch-65, 3'b000 + 8*23 }]; // W
-            5'd24: line = charrom[yoff + {ch-65, 3'b000 + 8*24 }]; // X
-            5'd25: line = charrom[yoff + {ch-65, 3'b000 + 8*25 }]; // Y
-            5'd26: line = charrom[yoff + {ch-65, 3'b000 + 8*26 }]; // Z
-            5'd27: line = charrom[yoff + {ch-65, 3'b000 + 8*27 }]; // *
-            5'd28: line = charrom[yoff + {ch-65, 3'b000 + 8*28 }]; // #
+            6'd1: line = charrom[yoff + {ch-65, 3'b000 + 8*1 }];  // A
+            6'd2: line = charrom[yoff + {ch-65, 3'b000 + 8*2 }];  // B
+            6'd3: line = charrom[yoff + {ch-65, 3'b000 + 8*3 }];  // C
+            6'd4: line = charrom[yoff + {ch-65, 3'b000 + 8*4 }];  // D
+            6'd5: line = charrom[yoff + {ch-65, 3'b000 + 8*5 }];  // E
+            6'd6: line = charrom[yoff + {ch-65, 3'b000 + 8*6 }];  // F
+            6'd7: line = charrom[yoff + {ch-65, 3'b000 + 8*7 }];  // G
+            6'd8: line = charrom[yoff + {ch-65, 3'b000 + 8*8 }];  // H
+            6'd9: line = charrom[yoff + {ch-65, 3'b000 + 8*9 }];  // I
+            6'd10: line = charrom[yoff + {ch-65, 3'b000 + 8*10 }]; // J
+            6'd11: line = charrom[yoff + {ch-65, 3'b000 + 8*11 }]; // K
+            6'd12: line = charrom[yoff + {ch-65, 3'b000 + 8*12 }]; // L
+            6'd13: line = charrom[yoff + {ch-65, 3'b000 + 8*13 }]; // M
+            6'd14: line = charrom[yoff + {ch-65, 3'b000 + 8*14 }]; // N
+            6'd15: line = charrom[yoff + {ch-65, 3'b000 + 8*15 }]; // O
+            6'd16: line = charrom[yoff + {ch-65, 3'b000 + 8*16 }]; // P
+            6'd17: line = charrom[yoff + {ch-65, 3'b000 + 8*17 }]; // Q
+            6'd18: line = charrom[yoff + {ch-65, 3'b000 + 8*18 }]; // R
+            6'd19: line = charrom[yoff + {ch-65, 3'b000 + 8*19 }]; // S
+            6'd20: line = charrom[yoff + {ch-65, 3'b000 + 8*20 }]; // T
+            6'd21: line = charrom[yoff + {ch-65, 3'b000 + 8*21 }]; // U
+            6'd22: line = charrom[yoff + {ch-65, 3'b000 + 8*22 }]; // V
+            6'd23: line = charrom[yoff + {ch-65, 3'b000 + 8*23 }]; // W
+            6'd24: line = charrom[yoff + {ch-65, 3'b000 + 8*24 }]; // X
+            6'd25: line = charrom[yoff + {ch-65, 3'b000 + 8*25 }]; // Y
+            6'd26: line = charrom[yoff + {ch-65, 3'b000 + 8*26 }]; // Z
+            6'd27: line = charrom[yoff + {ch-65, 3'b000 + 8*27 }]; // *
+            6'd28: line = charrom[yoff + {ch-65, 3'b000 + 8*28 }]; // #
+				
+				6'd29: line = charrom[yoff + {ch-65, 3'b000 + 8*29 }]; // A underlined
+            6'd30: line = charrom[yoff + {ch-65, 3'b000 + 8*30 }]; // B underlined
+            6'd31: line = charrom[yoff + {ch-65, 3'b000 + 8*31 }]; // C underlined
+            6'd32: line = charrom[yoff + {ch-65, 3'b000 + 8*32 }]; // D underlined
+            6'd33: line = charrom[yoff + {ch-65, 3'b000 + 8*33 }]; // E underlined
+            6'd34: line = charrom[yoff + {ch-65, 3'b000 + 8*34 }]; // F underlined
+            6'd35: line = charrom[yoff + {ch-65, 3'b000 + 8*35 }]; // G underlined
+            6'd36: line = charrom[yoff + {ch-65, 3'b000 + 8*36 }]; // H underlined
+            6'd37: line = charrom[yoff + {ch-65, 3'b000 + 8*37 }]; // I underlined
+            6'd38: line = charrom[yoff + {ch-65, 3'b000 + 8*38 }]; // J underlined
+            6'd39: line = charrom[yoff + {ch-65, 3'b000 + 8*39 }]; // K underlined
+            6'd40: line = charrom[yoff + {ch-65, 3'b000 + 8*40 }]; // L underlined
+            6'd41: line = charrom[yoff + {ch-65, 3'b000 + 8*41 }]; // M underlined
+            6'd42: line = charrom[yoff + {ch-65, 3'b000 + 8*42 }]; // N underlined
+            6'd43: line = charrom[yoff + {ch-65, 3'b000 + 8*43 }]; // O underlined
+            6'd44: line = charrom[yoff + {ch-65, 3'b000 + 8*44 }]; // P underlined
+            6'd45: line = charrom[yoff + {ch-65, 3'b000 + 8*45 }]; // Q underlined
+            6'd46: line = charrom[yoff + {ch-65, 3'b000 + 8*46 }]; // R underlined
+            6'd47: line = charrom[yoff + {ch-65, 3'b000 + 8*47 }]; // S underlined
+            6'd48: line = charrom[yoff + {ch-65, 3'b000 + 8*48 }]; // T underlined
+            6'd49: line = charrom[yoff + {ch-65, 3'b000 + 8*49 }]; // U underlined
+            6'd50: line = charrom[yoff + {ch-65, 3'b000 + 8*50 }]; // V underlined
+            6'd51: line = charrom[yoff + {ch-65, 3'b000 + 8*51 }]; // W underlined
+            6'd52: line = charrom[yoff + {ch-65, 3'b000 + 8*52 }]; // X underlined
+            6'd53: line = charrom[yoff + {ch-65, 3'b000 + 8*53 }]; // Y underlined
+            6'd54: line = charrom[yoff + {ch-65, 3'b000 + 8*54 }]; // Z underlined
+            6'd55: line = charrom[yoff + {ch-65, 3'b000 + 8*55 }]; // * underlined
             default: line = charrom[yoff + {ch-65, 3'b000 + 8*0 }]; // ESPACIO
         endcase
     end else begin
